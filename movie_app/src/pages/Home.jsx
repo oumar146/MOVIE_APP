8907ef2512f785ba7f6586fa07fb9a6ab8a890ee
SALL Oumar <119567549+oumar146@users.noreply.github.com>
2024-05-28 15:50:23 +0200

    Add v1 Banner
    Add background and description of movie

--- movie_app/src/pages/Home.jsx
+++ movie_app/src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import Header from '../components/Header';
-
+import Banner from '../components/Banner';
 function Home() {
     return (
         <div>
-            <Header />
+            <Banner>
+                <Header />
+            </Banner>
         </div>
     );
 }
