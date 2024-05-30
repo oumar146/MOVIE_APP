48040657ded5bb6421bb23175c4a70b9d3c8ab0e
SALL Oumar <119567549+oumar146@users.noreply.github.com>
2024-05-30 14:40:07 +0200

    Add API call
    - casting
    - movies by genres
    - similar movies

--- movie_app/src/components/TMDB_API/MovieByGenre.jsx
+++ movie_app/src/components/TMDB_API/MovieByGenre.jsx
@@ -2,14 +2,14 @@ import { useEffect } from 'react'
 const { API_KEY_TMDB, TOKEN } = require('../../config.json');
 
 /*Ce composant à besoins des props suivant : id = id du film et setData = fonction pour changer la valeur d'un state */
-const MovieActors = (props) => {
+const MovieByGenre = (props) => {
 
     useEffect(() => {
 
         if (!props.id) return
         async function fetchData() {
 
-            const response = await fetch(`http://api.themoviedb.org/3/movie/${props.id}/casts?api_key=${API_KEY_TMDB}`
+            const response = await fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY_TMDB}&with_genres=${props.id}`
                 , {
                     method: 'GET',
                     headers: {
@@ -18,7 +18,7 @@ const MovieActors = (props) => {
                     },
                 })
             const data = await response.json()
-            props.setData(data.cast)
+            props.setData(data.results)
         }
 
         fetchData()
@@ -35,4 +35,4 @@ const MovieActors = (props) => {
 
 }
 
-export default MovieActors
+export default MovieByGenre
--- movie_app/src/pages/Home.jsx
+++ movie_app/src/pages/Home.jsx
@@ -1,9 +1,14 @@
+import React, { useState } from 'react';
 import Header from '../components/Header';
+import MovieByGenre from '../components/TMDB_API/MovieByGenre';
+import MovieGenres from '../components/TMDB_API/MovieGenres';
 
 function Home() {
+    const [test, setTest] = useState([])
     return (
         <div>
             <Header />
+            <MovieByGenre id='12' setData={setTest} />
         </div>
     );
 }
